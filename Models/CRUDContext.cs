c6c86d9dcc9cb1af5ba2c2afcd2a934f80d9b939
Krupali Vithlani <45282063+vithlani@users.noreply.github.com>
2019-10-22 15:37:52 +0530

    Update CRUDContext.cs

--- Models/CRUDContext.cs
+++ Models/CRUDContext.cs
@@ -18,17 +18,6 @@ namespace UniCourses.Models
         public virtual DbSet<Courses> Courses { get; set; }
         public virtual DbSet<University> University { get; set; }
 
-        
-
-       /* protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=KRUPS\\MYSQL;Database=CRUD;Trusted_Connection=True;");
-            }
-        }
-*/
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Courses>(entity =>
